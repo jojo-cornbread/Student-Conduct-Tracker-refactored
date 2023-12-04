00a47b0b8e28359e5354a14a2ac8988b70b401c9
jojo-cornbread <josiahrambally13@gmail.com>
2023-12-04 02:23:17 +0000

    attempt to fix FK dependency issue #2

--- App/models/karma.py
+++ App/models/karma.py
@@ -9,7 +9,7 @@ class Karma(db.Model):
   rank = db.Column(db.Integer, nullable=False, default=-99)
 
   # set up FK relationship with student
-  studentID = db.Column(db.String(10), db.ForeignKey('student.ID', use_alter=True), nullable=True)
+  studentID = db.Column(db.String(10), db.ForeignKey('student.ID', name='fk_studentID', use_alter=True), nullable=True)
 
   # Foreign key to student
   # student_id = db.Column(db.Integer, db.ForeignKey('student.ID'))
